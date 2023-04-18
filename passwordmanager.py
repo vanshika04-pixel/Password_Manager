57b5ab85578c5d626c2a2d947c96bf02ed47398e
vanshika04-pixel <vanshikasaxena0412@gmail.com>
2023-04-18 13:33:25 +0530

    First commit

---
+++ passwordmanager.py
@@ -0,0 +1,41 @@
+from cryptography.fernet import Fernet
+"""
+def write_key():
+    key=Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+    """
+def load_key():
+    key_file=open("key.key","rb")
+    key=key_file.read()
+    key_file.close()
+    return key
+
+master_pwd=input("What is the master password ")
+key=load_key()+master_pwd.encode()
+fer=Fernet(key)
+
+def view():
+    with open("password.txt") as f:
+        for line in f.readlines():
+            data=line.rstrip()
+            user, passwd=data.split("|")
+            print("User: ",user," Password: ",fer.decrypt(passwd.encode()).decode())
+def add():
+    name=input("Account Name: ")
+    pwd=input("Password: ")
+    
+    with open('password.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+while True:
+    mode=input("would you like to add a new password or view an existing one (view/add)?\n press q to quit ").lower()
+    if mode=="q":
+        break
+    if mode=="view":
+        view()
+    elif mode=="add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
+    
